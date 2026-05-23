4b4c9efd64a4ea6740968160f86d1a24aa8e7d73
Kushagra-Dobriyal <kushagra.dobriyal@gmail.com>
2026-05-23 20:25:37 +0530

    Another commit

--- client/app/config.js
+++ client/app/config.js
@@ -1,6 +1,6 @@
 const API_URL = 
-  typeof window !== 'undefined' 
+  typeof window !== 'undefined'
     ? `${window.location.protocol}//${window.location.hostname}:5000`
-    : 'http://168.144.68.231:5000';
+    : process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5000';
 
 export default API_URL;
